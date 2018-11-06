ab96370e1232e06f3d8e91e108aac32222660027
Darren Jones <darren.jones@kohls.com>
2018-11-06 15:54:19 -0600

    fixed likes var

--- examples/LikeBot/LikeBot.js
+++ examples/LikeBot/LikeBot.js
@@ -265,25 +265,19 @@ function getCurrentName(command) {
 function doLikes(command, likes) {
     let keyword = command.keyword;
     let thing = command.args.join(" ");
-    let likes;
-    if (keyword == 'like' || keyword == 'dislike') {
-        likes = keyword == 'like' ? 1 : -1;
-     } else {
-         likes = keyword == 'love' ? 2 : -2;
-     }
-     if (currName == thing) {
-         message(command, "You cannot " + keyword + " yourself, silly willy!");
-         return;
-     }
-     run("INSERT INTO things(name, likes, fightTime, human, wagerName, wagerLikes) VALUES('" + thing + "', " + likes + ", 0, 0, 0, 0) ON CONFLICT(name)"
- 	    + " DO UPDATE SET likes=(likes + " + likes + ")");
-     db.get("SELECT * FROM things WHERE name=?", [thing], (err, row) => {
-         if (err) {
-             console.error(err.message);
-             return;
-         }
-         let msg = (currName.charAt(0).toUpperCase() + currName.slice(1)) + " " + keyword + "d '" + (thing.charAt(0).toUpperCase() + thing.slice(1))
-		+ "' and added " + likes + " likes. '" + (thing.charAt(0).toUpperCase() + thing.slice(1)) + "' now has " + row.likes + " like(s).";
+    if (currName == thing) {
+        message(command, "You cannot " + keyword + " yourself, silly willy!");
+        return;
+    }
+    run("INSERT INTO things(name, likes, fightTime, human, wagerName, wagerLikes) VALUES('" + thing + "', " + likes + ", 0, 0, 0, 0) ON CONFLICT(name)"
+ 	        + " DO UPDATE SET likes=(likes + " + likes + ")");
+    db.get("SELECT * FROM things WHERE name=?", [thing], (err, row) => {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        let msg = (currName.charAt(0).toUpperCase() + currName.slice(1)) + " " + keyword + "d '" + (thing.charAt(0).toUpperCase() + thing.slice(1))
+		        + "' and added " + likes + " likes. '" + (thing.charAt(0).toUpperCase() + thing.slice(1)) + "' now has " + row.likes + " like(s).";
         return row ? message(command, msg) : message(command, "wot happened?");
     });
 }
