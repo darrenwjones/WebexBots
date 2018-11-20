74acc6daea332ee7c19d86f0f8102aa24d2901cd
Darren Jones <darren.jones@kohls.com>
2018-11-20 09:22:30 -0600

    test

--- examples/LikeBot/LikeBot.js
+++ examples/LikeBot/LikeBot.js
@@ -32,7 +32,7 @@ var db = new sqlite3.Database('/home/darrenwjones06/WebexBots/examples/LikeBot/L
 //
 //
 bot.onCommand("fallback", function (command) {
-    message(command, "no u bro beans try 37");
+    message(command, "no u bro beans try 39");
 });
 
 bot.onCommand("help", function (command) {
